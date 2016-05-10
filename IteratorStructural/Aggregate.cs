e720bb4073ccf855eb24aaf8f3e0cc9a0e615c07
Connor Boesch <Connor_Boesch@yahoo.com>
2016-05-10 16:59:10 -0500

    Abstract classes.

---
+++ IteratorStructural/Aggregate.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IteratorStructural {
+	abstract class Aggregate {
+		public abstract Iterator CreateIterator();
+	}
+}
---
+++ IteratorStructural/Iterator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IteratorStructural {
+	abstract class Iterator {
+		public abstract object First();
+		public abstract object Next();
+		public abstract bool isDone();
+		public abstract object CurrentItem();
+	}
+}
